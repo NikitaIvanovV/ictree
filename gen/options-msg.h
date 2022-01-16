68782287b9e327c4999eec0b59b0e175410ca640
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-01-16 18:15:49 +0500

    Add info about --fold option

--- gen/options-msg.h
+++ gen/options-msg.h
@@ -1,9 +1,14 @@
 #define OPTIONS_MSG \
 "\n" \
 "Options:\n" \
+"       --fold, -f\n" \
+"              Fold directories by default.\n" \
+"\n" \
 "       --help, -h\n" \
-"              Print a help message and exit.\n" \
+"              Print  a  help  message   and\n" \
+"              exit.\n" \
 "\n" \
 "       --version, -v\n" \
-"              Display version information and exit.\n" \
+"              Display  version  information\n" \
+"              and exit.\n" \
 "\n"
