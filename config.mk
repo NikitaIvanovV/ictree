403a15d82722076aa02d6d062aec7f151530d0d2
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-01-17 15:51:24 +0500

    Remove license notice from config.mk

--- config.mk
+++ config.mk
@@ -1,21 +1,3 @@
-#
-# Copyright 2022 Nikita Ivanov
-#
-# This file is part of ictree
-#
-# ictree is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# ictree is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# ictree. If not, see <https://www.gnu.org/licenses/>.
-#
-
 PREFIX    ?= /usr/local
 BINPREFIX := $(DESTDIR)$(PREFIX)/bin
 MANPREFIX := $(DESTDIR)$(PREFIX)/share/man
