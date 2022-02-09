76395832e596c72aa3ab1c94a88e33ce08ba3b96
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-02-09 12:26:53 +0500

    Add docs about --separator option

--- gen/options-msg.h
+++ gen/options-msg.h
@@ -4,6 +4,9 @@
 "       --fold, -f\n" \
 "              Fold directories by default.\n" \
 "\n" \
+"       --separator=<C>, -s<C>\n" \
+"              Set directory separator to C.  / is the default value\n" \
+"\n" \
 "       --help, -h\n" \
 "              Print a help message and exit.\n" \
 "\n" \
