44e6958d542327e2ee5c96c85ab230bea738df68
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-02-10 21:53:12 +0500

    Space command can only fold

--- src/ictree.c
+++ src/ictree.c
@@ -676,7 +676,7 @@ static UpdScrSignal handle_key(struct tb_event ev)
 
     switch (ev.ch) {
     case ' ':
-        CONTROL_ACTION(toggle_fold(); scroll_y(SCROLL_Y));
+        CONTROL_ACTION(fold(); scroll_y(SCROLL_Y));
     case 'c':
         CONTROL_ACTION(goto_parent(); fold());
     case 'q':
