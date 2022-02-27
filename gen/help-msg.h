94bc666489d1a7132b5a17751a13d174df6a87d0
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-02-27 19:21:01 +0500

    Fix: forgot to add file

---
+++ gen/help-msg.h
@@ -0,0 +1,12 @@
+#define HELP_MSG \
+"Usage:	ictree [OPTION...]  [FILE]" "\n" \
+"" "\n" \
+"Options:" "\n" \
+"       --fold, -f" "\n" \
+"              Fold directories by default." "\n" \
+"       --separator=<C>, -s <C>" "\n" \
+"              Set directory separator to C.  / is the default value." "\n" \
+"       --help, -h" "\n" \
+"              Print a help message and exit." "\n" \
+"       --version, -v" "\n" \
+"              Display version information and exit."
